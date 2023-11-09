22e9c3b717150d7c7ebe6969b1237b85e7bdda31
youchen <youchenchen2023@gmail.com>
2023-11-10 00:06:57 +0800

    std::vector<int> heartReagions;

--- femheart.cpp
+++ femheart.cpp
@@ -98,6 +98,8 @@ int main(int argc, char *argv[])
    int dim = mesh->Dimension();
 
    //Fill in the MatrixElementPiecewiseCoefficients
+   std::vector<int> heartReagions;
+   objectGet(obj, "heart_regions", heartReagions);
    std::vector<double> sigma_m;
    objectGet(obj,"sigma_m",sigma_m);
 
@@ -351,7 +353,7 @@ int main(int argc, char *argv[])
          double value = sigma_m[jj]*dt/Bm/Cm;
          sigma_m_vec[jj] = value;
       }
-      sigma_m_coeffs.heartConductivities_[0] = sigma_m_vec;
+      sigma_m_coeffs.heartConductivities_[heartRegions[0]] = sigma_m_vec;
    }
    //Set up the bilinear form a(.,.) on the finite element space
    //orresponding to the Laplacian operator -Delta, by adding the Diffusion
